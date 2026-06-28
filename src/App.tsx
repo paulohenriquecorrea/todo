2526baaefd3697535752ff364b2c40e8e7e98e3b
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-06-28 17:07:30 -0400

    Criado Componente Button

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import PlusIcon from './assets/icons/Plus-Regular.svg?react';
 import spinnerIcon from './assets/icons/spinner.svg?react';
 import XIcon from './assets/icons/x.svg?react';
 import Badge from './components/badge';
+import Button from './components/button';
 
 export default function App() {
 
@@ -35,6 +36,9 @@ export default function App() {
       <div>
         <Badge variant={'secondary'}>5</Badge>
         <Badge>2 de 5</Badge>
+      </div>
+      <div>
+        <Button icon={PlusIcon}>Nova tarefa</Button>
       </div>
     </div>
   )
