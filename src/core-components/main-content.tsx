63241d2d881f270102644100eadb07cd93d1f6cc
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-07-14 00:15:22 -0400

    Criado componente TasksSummary

--- src/core-components/main-content.tsx
+++ src/core-components/main-content.tsx
@@ -1,5 +1,6 @@
 import {cx} from "class-variance-authority";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface MainContentProps extends React.ComponentProps<"main">{}
 
 export default function MainContent({
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,6 +1,13 @@
 import Container from "../components/container";
+import TasksSummary from "../core-components/tasks-summary";
 
 
 export function PageHome(){
-    return <Container>PÁGINA HOME!</Container>
+    return (
+        <Container as="article" className="space-y-3">
+            <header className="flex items-center justify-between">
+                <TasksSummary />
+            </header>
+        </Container>
+    )
 }
