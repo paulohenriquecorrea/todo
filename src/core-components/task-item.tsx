dc6db1329bff99d2c64ba4842487a733d6393f3b
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-07-25 13:48:46 -0400

    Finalizado: Coletando dados do Formulário

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -20,6 +20,8 @@ export default function TaskItem({task}: TaskItemProps ) {
         task?.state === TaskState.Creating
     );
 
+    const [taskTitle, setTaskTitle] = React.useState("");
+
     function handleEditTask() {
         setIsEditing(true);
     }
@@ -28,10 +30,23 @@ export default function TaskItem({task}: TaskItemProps ) {
         setIsEditing(false);
     }
 
+    function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+        setTaskTitle(e.target.value || "")
+
+    }
+
+    function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log({id: task.id, task: taskTitle})
+        // Chamada para a função de atualizar
+        setIsEditing(false);
+
+    }
+
     return (
-        <Card size="md" className="flex items-center gap-4">
+        <Card size="md">
             {!isEditing ? (
-                <>
+                <div className="flex items-center gap-4">
                     <InputCheckbox
                         value={task?.concluded?.toString()}
                         checked={task?.concluded} 
@@ -49,10 +64,10 @@ export default function TaskItem({task}: TaskItemProps ) {
                             onClick={handleEditTask} 
                         />
                     </div>
-                </>
+                </div>
             )   : (
-                <>
-                    <InputText />
+                <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+                    <InputText className="flex-1" onChange={handleChangeTaskTitle} required autoFocus/>
                     <div className="flex gap-1">
                         <ButtonIcon 
                             icon={XIcon} 
@@ -61,7 +76,7 @@ export default function TaskItem({task}: TaskItemProps ) {
                         />
                         <ButtonIcon icon={CheckIcon} variant="primary" />
                     </div>
-                </>
+                </form>
             )}
         </Card>
     )
--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -3,6 +3,7 @@ import PlusIcon from "../assets/icons/Plus-Regular.svg?react"
 import TaskItem from "./task-item";
 import useTasks from "../hooks/use-tasks";
 import useTask from "../hooks/use-task";
+import { TaskState } from "../models/task";
 
 
 export default function TasksList() {
@@ -20,6 +21,7 @@ export default function TasksList() {
                     icon={PlusIcon} 
                     className="w-full"
                     onClick={handleNewTask}
+                    disabled={tasks.some((task) => task.state === TaskState.Creating)}
                 >
                     Nova Tarefa
                 </Button>
