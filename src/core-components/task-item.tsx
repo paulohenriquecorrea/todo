55423534278a734b70cd4e5b4dbb5b08c6ea1724
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2026-07-31 23:12:11 -0400

    Finalizada atualização de tarefa

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -9,6 +9,7 @@ import XIcon from '../assets/icons/x.svg?react';
 import CheckIcon from '../assets/icons/Check-Regular.svg?react';import InputText from "../components/input-text";
 import { TaskState, type Task } from "../models/task";
 import { cx } from "class-variance-authority";
+import useTask from "../hooks/use-task";
 
 interface TaskItemProps {
  task: Task;
@@ -20,7 +21,8 @@ export default function TaskItem({task}: TaskItemProps ) {
         task?.state === TaskState.Creating
     );
 
-    const [taskTitle, setTaskTitle] = React.useState("");
+    const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+    const {updateTask} = useTask();
 
     function handleEditTask() {
         setIsEditing(true);
@@ -37,8 +39,7 @@ export default function TaskItem({task}: TaskItemProps ) {
 
     function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        console.log({id: task.id, task: taskTitle})
-        // Chamada para a função de atualizar
+        updateTask(task.id, {title: taskTitle});
         setIsEditing(false);
 
     }
@@ -67,7 +68,13 @@ export default function TaskItem({task}: TaskItemProps ) {
                 </div>
             )   : (
                 <form onSubmit={handleSaveTask} className="flex items-center gap-4">
-                    <InputText className="flex-1" onChange={handleChangeTaskTitle} required autoFocus/>
+                    <InputText
+                    value={taskTitle} 
+                    className="flex-1" 
+                    onChange={handleChangeTaskTitle} 
+                    required 
+                    autoFocus
+                    />
                     <div className="flex gap-1">
                         <ButtonIcon 
                             icon={XIcon} 
