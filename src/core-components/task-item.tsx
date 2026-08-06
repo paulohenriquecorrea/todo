e97613965bd502a93a749b3335c04a6305aaa263
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2026-08-06 17:07:02 -0400

    Conluída alteração de status da tarefa

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -22,7 +22,7 @@ export default function TaskItem({task}: TaskItemProps ) {
     );
 
     const [taskTitle, setTaskTitle] = React.useState(task.title || "");
-    const {updateTask} = useTask();
+    const {updateTask, updateTaskStatus} = useTask();
 
     function handleEditTask() {
         setIsEditing(true);
@@ -44,13 +44,19 @@ export default function TaskItem({task}: TaskItemProps ) {
 
     }
 
+    function handleChangeTaskStatus(e: React.ChangeEvent<HTMLInputElement>) {
+        const checked = e.target.checked;
+
+        updateTaskStatus(task.id, checked)
+    }
+
     return (
         <Card size="md">
             {!isEditing ? (
                 <div className="flex items-center gap-4">
                     <InputCheckbox
-                        value={task?.concluded?.toString()}
-                        checked={task?.concluded} 
+                        checked={task?.concluded}
+                        onChange={handleChangeTaskStatus}
                     />
                     <Text className={cx("flex-1", {
                         'line-through': task?.concluded,
