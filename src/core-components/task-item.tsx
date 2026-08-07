a9360ef7c4f9f2a450fdd5da156991610518961a
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2026-08-07 15:57:41 -0400

    Adicionada funcionalidade Deletar Tarefa

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -22,13 +22,16 @@ export default function TaskItem({task}: TaskItemProps ) {
     );
 
     const [taskTitle, setTaskTitle] = React.useState(task.title || "");
-    const {updateTask, updateTaskStatus} = useTask();
+    const {updateTask, updateTaskStatus, deleteTask} = useTask();
 
     function handleEditTask() {
         setIsEditing(true);
     }
 
     function handleExitEditTask() {
+        if (task.state === TaskState.Creating) {
+            deleteTask(task.id);
+        }
         setIsEditing(false);
     }
 
@@ -50,6 +53,10 @@ export default function TaskItem({task}: TaskItemProps ) {
         updateTaskStatus(task.id, checked)
     }
 
+    function handleDeleteTask() {
+        deleteTask(task.id);
+    }
+
     return (
         <Card size="md">
             {!isEditing ? (
@@ -64,7 +71,11 @@ export default function TaskItem({task}: TaskItemProps ) {
                         {task?.title}
                     </Text>
                     <div className="flex gap-1">
-                        <ButtonIcon icon={TrashIcon} variant="tertiary" />
+                        <ButtonIcon 
+                            icon={TrashIcon} 
+                            variant="tertiary" 
+                            onClick={handleDeleteTask} 
+                        />
                         <ButtonIcon 
                             icon={Pencil} 
                             variant="tertiary" 
